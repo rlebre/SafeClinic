4a08b61aacf36456087ddeab4d141500efca5139
Rui Lebre <ruilebre@ua.pt>
2020-01-16 17:11:45 +0000

    improved aot simulation

--- src/main/java/pteidsample/AOTSimulation.java
+++ src/main/java/pteidsample/AOTSimulation.java
@@ -1,5 +1,11 @@
 package pteidsample;
 
+import ieeta.aot.Authorization;
+import ieeta.aot.Utils;
+import ieeta.aot.node.NodeServer;
+import ieeta.aot.node.NodeSession;
+import ieeta.aot.terminal.Terminal;
+import ieeta.aot.terminal.TerminalSession;
 import pt.gov.cartaodecidadao.*;
 
 import java.io.ByteArrayInputStream;
@@ -12,53 +18,59 @@ import java.security.cert.CertificateFactory;
 public class AOTMainTest {
 
     public static void main(String[] args) {
-        //todo();
-        try {
-            todo2();
-        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
-            e.printStackTrace();
+        final Terminal term = Utils.genTerminal();
+        final NodeServer node = Utils.genNodeServer();
+        final CitizenCard cc = CitizenCard.getInstance();
+
+        if (cc == null) {
+            System.err.println("Error initializing Portuguese Citizen Card");
+            System.exit(1);
         }
-//        final Terminal term = Utils.genTerminal();
-//        final NodeServer node = Utils.genNodeServer();
-//
-//        // Assuming that terminal key and operator key is already certified by the node
-//
-//        final TerminalSession tSession = term.genSession(node.pkey);
-//        final Authorization auth = tSession.authorize(data -> {
-//            System.out.println("Token: " + Utils.bytesToHex(data));
-//            System.out.println("SHA-256(Token): " + Utils.bytesToHex(Utils.hash(data)));
-//
-//            // TODO: perform a Citizens Card signature on "data" -> "sigBytes"
-//
-//            // return new ExtSignature(pubKey, sigBytes);
-//            return null;
-//        });
-//
-//        // TODO: authorization is sent via network
-//
-//        // TODO: in real applications both keys should be checked before binding the session (auth.termKey & auth.extSig.operKey)
-//        final NodeSession nSession = node.bindSession(auth, cdata -> {
-//            System.out.println("Token: " + Utils.bytesToHex(cdata.data));
-//
-//            //TODO: check if "cdata.time" is in acceptable range
-//            //TODO: perform a Citizens Card signature check on "cdata.data" using "cdata.extSig"
-//
-//            return true;
-//        });
-//
-//        // TODO: encrypt and send data Ek[data]
-//        final byte[] ciphertext = nSession.encrypt("Testing sending data!".getBytes());
-//
-//        // TODO: send Ek1[k2] when consent is confirmed
-//        tSession.setK(nSession.encK2);
-//        final byte[] plaintext = tSession.dencrypt(ciphertext);
-//
-//        System.out.println(new String(plaintext));
+
+        // Assuming that terminal key and operator key is already certified by the node
+        final TerminalSession tSession = term.genSession(node.pkey);
+        final Authorization auth = tSession.authorize(data -> {
+            System.out.println("Token: " + Utils.bytesToHex(data));
+            System.out.println("SHA-256(Token): " + Utils.bytesToHex(Utils.hash(data)));
+
+            // perform a Citizens Card signature on "data" -> "sigBytes"
+            byte[] sigBytes = cc.sign(Utils.hash(data), CitizenCard.SIGNATURE);
+            byte[] pubKey = cc.getSignaturePublicKey().getEncoded();
+
+            return new Authorization.ExtSignature(pubKey, sigBytes);
+        });
+
+        // TODO: authorization is sent via network
+
+        // TODO: in real applications both keys should be checked before binding the session (auth.termKey & auth.extSig.operKey)
+        final NodeSession nSession = node.bindSession(auth, cdata -> {
+            System.out.println("Token: " + Utils.bytesToHex(cdata.data));
+
+            //TODO: check if "cdata.time" is in acceptable range
+
+            // perform a Citizens Card signature check on "cdata.data" using "cdata.extSig"
+            try {
+                return cc.validateSignature(cdata.data, cdata.extSig.sig, CitizenCard.SIGNATURE);
+            } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
+                return false;
+            }
+        });
+
+        // TODO: encrypt and send data Ek[data]
+        final byte[] ciphertext = nSession.encrypt("Testing sending data!".getBytes());
+
+        // TODO: send Ek1[k2] when consent is confirmed
+        tSession.setK(nSession.encK2);
+        final byte[] plaintext = tSession.dencrypt(ciphertext);
+
+        System.out.println(new String(plaintext));
+
+        cc.close();
     }
 
 
     public static void todo2() throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
-        CC cc = CC.getInstance();
+        CitizenCard cc = CitizenCard.getInstance();
 
         if (cc == null) {
             System.err.println("Error initializing Portuguese Citizen Card");
@@ -66,8 +78,8 @@ public class AOTMainTest {
         }
 
         String data = "data_to_sign";
-        byte[] signature = cc.sign(data, CC.SIGNATURE);
-        System.out.println(cc.validateSignature(data, signature, CC.SIGNATURE));
+        byte[] signature = cc.sign(data, CitizenCard.SIGNATURE);
+        System.out.println(cc.validateSignature(data, signature, CitizenCard.SIGNATURE));
 
         cc.close();
     }
