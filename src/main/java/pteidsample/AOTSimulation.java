c1f67ef17245ad60ac8d7ce93ccd1bb127067f87
Rui Lebre <ruilebre@ua.pt>
2020-01-22 11:12:10 +0000

    adapted main API to be compliant with the new version

--- src/main/java/pteidsample/AOTSimulation.java
+++ src/main/java/pteidsample/AOTSimulation.java
@@ -1,6 +1,7 @@
 package pteidsample;
 
-import ieeta.aot.Authorization;
+import ieeta.aot.AuthRequest;
+import ieeta.aot.EncryptedData;
 import ieeta.aot.Utils;
 import ieeta.aot.node.NodeServer;
 import ieeta.aot.node.NodeSession;
@@ -18,33 +19,27 @@ public class AOTSimulation {
         final NodeServer node = Utils.genNodeServer();
         final CC cc = CC.getInstance();
 
-        if (cc == null) {
-            System.err.println("Error initializing Portuguese Citizen Card");
-            System.exit(1);
-        }
-
         // Assuming that terminal key and operator key is already certified by the node
-        final TerminalSession tSession = term.genSession(node.pkey);
-        final Authorization auth = tSession.authorize(data -> {
+
+        final TerminalSession tSession = term.genSession(data -> {
             System.out.println("Token: " + Utils.bytesToHex(data));
             System.out.println("SHA-256(Token): " + Utils.bytesToHex(Utils.hash(data)));
+
 
             // perform a Citizens Card signature on "data" -> "sigBytes"
             byte[] sigBytes = cc.sign(Utils.hash(data), CC.SIGNATURE_KEY_PAIR);
             byte[] pubKey = cc.getSignaturePublicKey().getEncoded();
 
-            return new Authorization.ExtSignature(pubKey, sigBytes);
+            return new AuthRequest.ExtSignature(pubKey, sigBytes);
         });
 
-        // TODO: authorization is sent via network
+        // TODO: AuthRequest is sent via network
 
         // TODO: in real applications both keys should be checked before binding the session (auth.termKey & auth.extSig.operKey)
-        final NodeSession nSession = node.bindSession(auth, cdata -> {
+        final NodeSession nSession = node.bindSession(tSession.req, cdata -> {
             System.out.println("Token: " + Utils.bytesToHex(cdata.data));
 
             //TODO: check if "cdata.time" is in acceptable range
-
-            // perform a Citizens Card signature check on "cdata.data" using "cdata.extSig"
 
             try {
                 PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(cdata.extSig.operKey));
@@ -54,11 +49,13 @@ public class AOTSimulation {
             }
         });
 
-        // TODO: encrypt and send data Ek[data]
-        final byte[] ciphertext = nSession.encrypt("Testing sending data!".getBytes());
+        // TODO: AuthResponse is sent via network
 
-        // TODO: send Ek1[k2] when consent is confirmed
-        tSession.setK(nSession.encK2);
+        // set k key from the node response
+        tSession.setK(node.pkey, nSession.resp);
+
+        // simulation of encrypted transmission
+        final EncryptedData ciphertext = nSession.encrypt("Testing sending data!".getBytes());
         final byte[] plaintext = tSession.dencrypt(ciphertext);
 
         System.out.println(new String(plaintext));
